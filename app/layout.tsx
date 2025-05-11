0ab49fcea19b2b2432e23820d5ab9500aaf7c02a
Berwamihigo <learncanvaa@gmail.com>
2025-05-11 17:03:05 +0200

    to make it available

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,10 +28,28 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css"/>
+        <title>Longtai</title>
+
+        <meta
+          name="description"
+          content="Longtai Auto is Rwanda’s trusted destination for quality car sales. Find affordable new and used vehicles, expert service, and unbeatable deals tailored to your needs."
+        />
+
+        <meta
+          name="keywords"
+          content="Longtai Auto, car sales Rwanda, used cars Rwanda, buy cars Kigali, auto dealership Rwanda"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.6.0/remixicon.css"
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
